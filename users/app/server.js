ba2cd96f9a15863926ca43f97cf488d04f2dcea8
sashmlv <sashmlv@yandex.ru>
2020-05-29 23:17:15 +0300

    Add server.

--- users/app/server.js
+++ users/app/server.js
@@ -1,30 +1,17 @@
 'use strict';
 
 const config = require( '@appliacation/config' ),
-   log = require( '@application/logger' );
+   log = require( '@application/logger' ),
+   app = require( './app' ),
+   server = require( 'http' ).createServer( app );
 
-const express = require( 'express' );
+server.on( 'error', error => log.error( error ));
 
-const server = {};
+server.listen(
 
-// server.listen({ host: config.HOST, port: config.PORT, }, ( err, address ) => {
-
-//    if( err ) {
-
-//       log.error( err );
-//       process.exit( 1 );
-//    };
-
-//    config.NODE_ENV === 'production' ? (
-
-//       console.log( log.prettyParams()),
-//       log.info( `Listen at: ${ config.HOST }:${ config.PORT }` )
-//    ) :
-//       console.log( JSON.stringify({
-
-//          HOST: config.HOST,
-//          PORT: config.PORT,
-//       }));
-// });
+   config.PORT,
+   config.HOST,
+   _=> log.info( `Server listen on: ${ config.HOST }:${ config.PORT }` )
+);
 
 module.exports = server;
